c5b907391763376bf7b7e5a7d3c7e0ee3eb6e00c
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-12-03 19:07:37 -0300

    💿 Update: hoverChangeDescription

--- configPort/js/functions.js
+++ configPort/js/functions.js
@@ -83,12 +83,57 @@ ScrollReveal().reveal('.resetContent', reset);
 // =============================================================================
 
 // =============================================================================
-// EVENTOS
-function textDesc(){
-    let desc = document.querySelector(".res")
-    
-    console.log(`${badget}`)
-    desc.innerText = 'OI'
+// EVENTOS DE DESCRIÇÃO
+function hoverChangeDescription(nameCard, text){
+    var changeDescription = document.querySelector('.res')
+
+    document.querySelector(nameCard).addEventListener("mouseover", () => {
+        changeDescription.innerText = text
+    })
+
+    document.querySelector(nameCard).addEventListener("mouseout", () => {
+        changeDescription.innerText = `*Passe o mouse ou clique em alguma insígnia.`
+    })
 }
-// EVENTOS
+
+hoverChangeDescription(
+    "#html",
+    "[HABILIDADE: 70%]"
+);
+
+hoverChangeDescription(
+    "#css",
+    "[HABILIDADE: 70%]"
+);
+
+hoverChangeDescription(
+    "#bootstrap",
+    "[HABILIDADE: 70%]"
+);
+
+hoverChangeDescription(
+    "#sass",
+    "[HABILIDADE: 70%]"
+);
+
+hoverChangeDescription(
+    "#javascript",
+    "[HABILIDADE: 50%]"
+);
+
+hoverChangeDescription(
+    "#python",
+    "[HABILIDADE: 90%]"
+);
+
+hoverChangeDescription(
+    "#markdown",
+    "[HABILIDADE: 90%]"
+)
+
+hoverChangeDescription(
+    "#git-plain",
+    "[HABILIDADE: 10%]"
+)
+// EVENTOS DE DESCRIÇÃO
 // =============================================================================
