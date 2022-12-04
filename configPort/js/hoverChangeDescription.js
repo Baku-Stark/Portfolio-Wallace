82e21f86e2cac47801961b663fd607ff01e66875
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-12-04 17:07:36 -0300

    💻 Update: Portfolio v1.0

--- configPort/js/hoverChangeDescription.js
+++ configPort/js/hoverChangeDescription.js
@@ -12,40 +12,40 @@ function hoverChangeDescription(nameCard, text){
 
 hoverChangeDescription(
     "#html",
-    "[HABILIDADE: 70%]"
+    "[HABILIDADE: 70%] * O HTML5 é responsável por criar a estrutura de um site (componente importante)."
 );
 
 hoverChangeDescription(
     "#css",
-    "[HABILIDADE: 70%]"
+    "[HABILIDADE: 70%] * Folhas CSS servem para estilizar o site."
 );
 
 hoverChangeDescription(
     "#bootstrap",
-    "[HABILIDADE: 70%]"
+    "[HABILIDADE: 70%] * Framework de CSS que serve como atalho para responsividade do site.."
 );
 
 hoverChangeDescription(
     "#sass",
-    "[HABILIDADE: 70%]"
+    "[HABILIDADE: 70%] * Extensão de código, servindo para turbinar as CSS."
 );
 
 hoverChangeDescription(
     "#javascript",
-    "[HABILIDADE: 50%]"
+    "[HABILIDADE: 50%] * Linguagem de programação multi-uso, mas utilizo na área de desenvolvimento Front-End"
 );
 
 hoverChangeDescription(
     "#python",
-    "[HABILIDADE: 90%]"
+    "[HABILIDADE: 90%] * Python é uma linguagem de programção Back-End. Uma excelente escolha para novatos."
 );
 
 hoverChangeDescription(
     "#markdown",
-    "[HABILIDADE: 90%]"
+    "[HABILIDADE: 100%] * Linguagem de progrmação para estilização e formatação textual."
 )
 
 hoverChangeDescription(
     "#git-plain",
-    "[HABILIDADE: 10%]"
+    "[HABILIDADE: 45%] * Git é um software de controle distruibuído. No caminho para manipular o Git por completo."
 )
