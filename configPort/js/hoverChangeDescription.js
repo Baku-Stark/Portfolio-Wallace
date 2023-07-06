c3d8253d696fa9ac04d56c7ad0da2e13b0e3a261
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-07-06 19:10:06 -0300

    💻 Update: Portfolio v1-0

--- configPort/js/hoverChangeDescription.js
+++ configPort/js/hoverChangeDescription.js
@@ -12,55 +12,55 @@ function hoverChangeDescription(nameCard, text){
 
 hoverChangeDescription(
     "#html",
-    "[APRENDIZADO: 100%] * HTML5 é responsável por criar a estrutura de um site (componente importante)."
+    "[CURSO: 100%] * HTML5 é responsável por criar a estrutura de um site (componente importante)."
 );
 
 hoverChangeDescription(
     "#css",
-    "[APRENDIZADO: 100%] * CSS serve para estilizar o site."
+    "[CURSO: 100%] * CSS serve para estilizar o site."
 );
 
 hoverChangeDescription(
     "#bootstrap",
-    "[APRENDIZADO: 100%] * Framework de CSS que serve como atalho para responsividade do site."
+    "[CURSO: 100%] * Framework de CSS que serve como atalho para responsividade do site."
 );
 
 hoverChangeDescription(
     "#sass",
-    "[APRENDIZADO: 100%] * SASS é uma extensão de código, servindo para turbinar as CSS."
+    "[CURSO: 100%] * SASS é uma extensão de código, servindo para turbinar as CSS."
 );
 
 hoverChangeDescription(
     "#javascript",
-    "[APRENDIZADO: 100%] * JavaScript é uma linguagem de programação multi-uso, mas utilizo na área de desenvolvimento Front-End"
+    "[CURSO: 100%] * JavaScript é uma linguagem de programação multi-uso, mas utilizo na área de desenvolvimento Front-End"
 );
 
 hoverChangeDescription(
     "#python",
-    "[APRENDIZADO: 100%] * Python é uma linguagem de programção Back-End. Uma excelente escolha para novatos."
+    "[CURSO: 100%] * Python é uma linguagem de programção Back-End. Uma excelente escolha para novatos."
 );
 
 hoverChangeDescription(
     "#markdown",
-    "[APRENDIZADO: 100%] * MarkDown é uma linguagem de progrmação para estilização e formatação textual."
+    "[CURSO: 100%] * MarkDown é uma linguagem de progrmação para estilização e formatação textual."
 )
 
 hoverChangeDescription(
     "#git-plain",
-    "[APRENDIZADO: 100%] * Git é um software de controle distruibuído. No caminho para manipular o Git por completo."
+    "[CURSO: 100%] * Git é um software de controle distruibuído. No caminho para manipular o Git por completo."
 )
 
 hoverChangeDescription(
     "#java",
-    "[APRENDIZADO: 90%] * Realizando projetos em Java (melhorias em breve no portfolio no Git Hub)."
+    "[CURSO: 90%] * Realizando projetos em Java (melhorias em breve no portfolio no Git Hub)."
 )
 
 hoverChangeDescription(
     "#cpp",
-    "[APRENDIZADO: 10%] * C++ ."
+    "[CURSO: 10%] * C++ ."
 )
 
 hoverChangeDescription(
     "#rust",
-    "[APRENDIZADO: 10%] * Rust."
+    "[CURSO: 10%] * Rust."
 )
