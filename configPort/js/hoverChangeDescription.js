6279af666c7ce4a463ff5d0d957dd22f77f1591a
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-07-06 18:57:36 -0300

    💻 Update: Portfolio v1-0

--- configPort/js/hoverChangeDescription.js
+++ configPort/js/hoverChangeDescription.js
@@ -12,50 +12,55 @@ function hoverChangeDescription(nameCard, text){
 
 hoverChangeDescription(
     "#html",
-    "[HABILIDADE: 70%] * HTML5 é responsável por criar a estrutura de um site (componente importante)."
+    "[APRENDIZADO: 100%] * HTML5 é responsável por criar a estrutura de um site (componente importante)."
 );
 
 hoverChangeDescription(
     "#css",
-    "[HABILIDADE: 70%] * CSS serve para estilizar o site."
+    "[APRENDIZADO: 100%] * CSS serve para estilizar o site."
 );
 
 hoverChangeDescription(
     "#bootstrap",
-    "[HABILIDADE: 70%] * Framework de CSS que serve como atalho para responsividade do site."
+    "[APRENDIZADO: 100%] * Framework de CSS que serve como atalho para responsividade do site."
 );
 
 hoverChangeDescription(
     "#sass",
-    "[HABILIDADE: 70%] * SASS é uma extensão de código, servindo para turbinar as CSS."
+    "[APRENDIZADO: 100%] * SASS é uma extensão de código, servindo para turbinar as CSS."
 );
 
 hoverChangeDescription(
     "#javascript",
-    "[HABILIDADE: 50%] * JavaScript é uma linguagem de programação multi-uso, mas utilizo na área de desenvolvimento Front-End"
+    "[APRENDIZADO: 100%] * JavaScript é uma linguagem de programação multi-uso, mas utilizo na área de desenvolvimento Front-End"
 );
 
 hoverChangeDescription(
     "#python",
-    "[HABILIDADE: 90%] * Python é uma linguagem de programção Back-End. Uma excelente escolha para novatos."
+    "[APRENDIZADO: 100%] * Python é uma linguagem de programção Back-End. Uma excelente escolha para novatos."
 );
 
 hoverChangeDescription(
     "#markdown",
-    "[HABILIDADE: 100%] * MarkDown é uma linguagem de progrmação para estilização e formatação textual."
+    "[APRENDIZADO: 100%] * MarkDown é uma linguagem de progrmação para estilização e formatação textual."
 )
 
 hoverChangeDescription(
     "#git-plain",
-    "[HABILIDADE: 45%] * Git é um software de controle distruibuído. No caminho para manipular o Git por completo."
+    "[APRENDIZADO: 100%] * Git é um software de controle distruibuído. No caminho para manipular o Git por completo."
+)
+
+hoverChangeDescription(
+    "#java",
+    "[APRENDIZADO: 90%] * Realizando projetos em Java (melhorias em breve no portfolio no Git Hub)."
 )
 
 hoverChangeDescription(
     "#cpp",
-    "[HABILIDADE: 10%] * C++ ."
+    "[APRENDIZADO: 10%] * C++ ."
 )
 
 hoverChangeDescription(
     "#rust",
-    "[HABILIDADE: 10%] * Rust."
+    "[APRENDIZADO: 10%] * Rust."
 )
